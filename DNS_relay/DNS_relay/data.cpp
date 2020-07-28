3073a3f5a5dab37e5a7bf3071735a5327508cb61
wyptaotao <1091193674@qq.com>
2020-07-28 23:06:13 +0800

    aaa

--- DNS_relay/DNS_relay/data.cpp
+++ DNS_relay/DNS_relay/data.cpp
@@ -1,3 +1,12 @@
 #include<stdio.h>
 #include<string.h>
+#include <winsock2.h> 
+int
 
+typedef struct {
+	unsigned short prev_ID;//与客户端通信使用的ID
+	int status;//标记当前对应的ID是否对客户端完成回传，0为未完成，1为已完成
+	SOCKADDR_IN client;//客户端的地址
+}ID_Binding_Unit;//记录对应ID的结构体
+
+ID_Binding_Unit[];
