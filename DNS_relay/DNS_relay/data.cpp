7a9bd01019c2c5e093d1038da23a6611093bab7b
wyptaotao <1091193674@qq.com>
2020-07-29 00:23:19 +0800

    摸鱼摸鱼摸鱼摸鱼

--- DNS_relay/DNS_relay/data.cpp
+++ DNS_relay/DNS_relay/data.cpp
@@ -1,12 +1,17 @@
 #include<stdio.h>
 #include<string.h>
 #include <winsock2.h> 
-int
+#include"def.h"
 
-typedef struct {
+
+typedef struct{
 	unsigned short prev_ID;//与客户端通信使用的ID
 	int status;//标记当前对应的ID是否对客户端完成回传，0为未完成，1为已完成
 	SOCKADDR_IN client;//客户端的地址
 }ID_Binding_Unit;//记录对应ID的结构体
 
-ID_Binding_Unit[];
+unsigned short Bind_ID(unsigned short ID, SOCKADDR_IN client) {
+	for (int i = 0; i < MAX_ID_TABLE_SIZE; i++) {
+		if()
+	}
+}
--- DNS_relay/DNS_relay/def.h
+++ DNS_relay/DNS_relay/def.h
@@ -1,1 +1,2 @@
 const int BUF_SIZE = 512;
+const int MAX_ID_TABLE_SIZE=16;//ID对应表最大容量
