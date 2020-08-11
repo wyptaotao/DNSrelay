b8072e9a63bfb6d73a9677c548526f997051fb59
wyptaotao <1091193674@qq.com>
2020-08-11 21:55:58 +0800

    ttl改为五秒

--- DNS_relay/DNS_relay/def.h
+++ DNS_relay/DNS_relay/def.h
@@ -1,5 +1,5 @@
 const int BUF_SIZE = 512;
 const int MAX_ID_TABLE_SIZE=16;//ID对应表最大容量
-const int TTL = 10;//生存期最大为十秒
+const int TTL = 5;//生存期最大为五秒
 int debug_level;
 const int DNS_Header_Size = 12;//DNS头字节数
