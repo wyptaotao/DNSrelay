4490274e12c6be2b35817ef2f91b02e9e3808799
wyptaotao <1091193674@qq.com>
2020-09-06 13:59:20 +0800

    新的结构体

--- DNS_relay/DNS_relay/data.h
+++ DNS_relay/DNS_relay/data.h
@@ -11,6 +11,7 @@ typedef struct {
 
 ID_Binding_Unit ID_Table[MAX_ID_TABLE_SIZE];//ID对应绑定表
 
+
 void Transfer_URL(char* buf, char* dest);//转换域名至可查询格式
 int If_Expired(ID_Binding_Unit a);//查询转发查询是否超时
 void Set_TTL(int ttl, ID_Binding_Unit* a);//设置转发查询生存时间
--- DNS_relay/DNS_relay/def.h
+++ DNS_relay/DNS_relay/def.h
@@ -1,10 +1,26 @@
-const int BUF_SIZE = 512;		//一个DNS包的最大长度
+const int MAX_BUF_SIZE = 512;		//一个DNS包的最大长度
 const int MAX_ID_TABLE_SIZE=16; //ID对应表最大容量
 const int TTL = 5;				//生存期最大为五秒
 const int DNS_HEARDER_SIZE = 12; //DNS头字节数
 const int DNS_PORT = 53;//DNS端口
 const int MAX_CACHE_SIZE=5;//CACHE表容量
 char DNS_Server_IP[16] = "10.3.9.4";//DNS服务器IP
+
+const int MAX_IP_LEN = 25;
+const int MAX_DOMAIN_NAME_LEN = 100;
+const int MAX_RECORDS_NUM = 2000;
+
+struct Record
+{
+	char ip[MAX_IP_LEN];
+	char dn[MAX_DOMAIN_NAME_LEN];
+};
+
+struct AllRecords
+{
+	Record record[MAX_RECORDS_NUM];
+	int count;
+};
 
 
 extern int debug_level;
